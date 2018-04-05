9e3bb59534ae6fb05aebb8269bdcfb89b1eb9bdd
OmarElkilany <omar.alkilani23@gmail.com>
2018-04-05 18:17:54 +0200

    feat: EX_PC_Calculation

---
+++ EX_PC_Calculation.v
@@ -0,0 +1,11 @@
+module EX_PC_Calculation(PC_in, offset, PC_out);
+
+input [31:0] PC_in;
+input [15:0] offset;
+
+output [31:0] PC_out;
+
+
+assign PC_out = PC_in + (offset << 2);
+
+endmodule
--- EX_Stage.v
+++ EX_Stage.v
@@ -1,3 +1,6 @@
+// two multiplexers were not implemented in the supporting modules
+// They are ALU operand 2 and writeDestination multiplexer
+// TODO Implement them here and connect the wiring properly
 module EX_Stage();
 
 
