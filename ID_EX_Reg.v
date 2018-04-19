a5286176516ed8ee3b65fab90423b5a48ed16e54
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-04-19 16:53:26 +0200

    fix: a minor error with ALUOp

--- ID_EX_Reg.v
+++ ID_EX_Reg.v
@@ -5,14 +5,16 @@ module ID_EX_Reg(input clk,
                  input [31:0] in_read_data1,
                  input [31:0] in_read_data2,
                  input [31:0] in_new_pc_value,
-                 input in_RegDst, in_RegWrite, in_ALUSrc, in_ALUOp, in_MemWrite, in_MemRead, in_MemToReg, in_PCSrc,
+                 input in_RegDst, in_RegWrite, in_ALUSrc, in_MemWrite, in_MemRead, in_MemToReg, in_PCSrc,
+                 input [2:0] in_ALUOp;
                  output reg [4:0] instr_bits_15_11,
                  output reg [4:0] instr_bits_20_16,
                  output reg [31:0] extended_bits,
                  output reg [31:0] read_data1,
                  output reg [31:0] read_data2,
                  output reg [31:0] new_pc_value,
-                 output reg RegDst, RegWrite, ALUSrc, ALUOp, MemWrite, MemRead, MemToReg, PCSrc);
+                 output reg RegDst, RegWrite, ALUSrc, MemWrite, MemRead, MemToReg, PCSrc,
+                 output reg [2:0] ALUOp);
 
 always @(posedge clk)
 begin
