2c445df068c7eb5ad7d6d44d4557843a3acddc46
Maqarios <maqarioswagdy@gmail.com>
2018-04-20 12:05:58 +0200

    style: changed variable names in IF/ID Reg

--- IF_ID_Reg.v
+++ IF_ID_Reg.v
@@ -1,7 +1,7 @@
 module (
     input CLK,                      // CLOCK
-    input [31:0] NEXT_INS,          // NEXT INSTRUCTION
-    input [31:0] CUR_INS,           // CURRENT INSTRUCTION
+    input [31:0] NEXT_INS_IN,       // NEXT INSTRUCTION INPUT
+    input [31:0] CUR_INS_IN,        // CURRENT INSTRUCTION INPUT
     output reg [31:0] NEXT_INS_OUT, // NEXT INSTRUCTION OUTPUT
     output reg [31:0] CUR_INS_OUT   // CURRENT INSTRUCTION OUTPUT
 );
