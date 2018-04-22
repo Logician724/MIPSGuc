fe702ad68cef37fe6bbaa021b78724ebe1f1bacd
Maqarios <maqarioswagdy@gmail.com>
2018-04-22 21:23:18 +0200

    fix: add memory to hold instructions

--- IF_ID_Reg.v
+++ IF_ID_Reg.v
@@ -1,15 +1,15 @@
 module (
-    input CLK,                      // CLOCK
-    input [31:0] NEXT_INS_IN,       // NEXT INSTRUCTION INPUT
-    input [31:0] CUR_INS_IN,        // CURRENT INSTRUCTION INPUT
-    output reg [31:0] NEXT_INS_OUT, // NEXT INSTRUCTION OUTPUT
-    output reg [31:0] CUR_INS_OUT   // CURRENT INSTRUCTION OUTPUT
+    input CLK,                          // CLOCK
+    input [31:0] NEXT_INS_ADR_IN,       // NEXT INSTRUCTION ADDRESS INPUT
+    input [31:0] CUR_INS_IN,            // CURRENT INSTRUCTION INPUT
+    output reg [31:0] NEXT_INS_ADR_OUT, // NEXT INSTRUCTION ADDRESS OUTPUT
+    output reg [31:0] CUR_INS_OUT       // CURRENT INSTRUCTION OUTPUT
 );
 
 always @(posedge CLK)
 begin
-  NEXT_INS_OUT <= NEXT_INS;
-  CUR_INS_OUT <= CUR_INS;
+  NEXT_INS_ADR_OUT <= NEXT_INS_ADR_IN;
+  CUR_INS_OUT <= CUR_INS_IN;
 end
 
 endmodule
