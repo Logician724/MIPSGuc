c6aec9d86b993d1141e8852846d9b74639ac7262
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-04-22 15:58:56 +0200

    fix: fixed the sign-extender

--- ID_Register_File.v
+++ ID_Register_File.v
@@ -26,12 +26,11 @@ output reg [31:0] extended_bits;
 
 reg [31:0] registers [31:0];
 
-// QUESTION: is this sufficient as an always list
 always @(*)
 begin
   read_data1 <= registers[instruction[25:21]];
   read_data2 <= registers[instruction[20:16]];
-  extended_bits <= 16'b0 + instruction[15:0];
+  extended_bits <= instruction[15] ? 16'hffff + instruction[15:0] : 16'b0 + instruction[15:0];
 end
 
 always @(posedge clk)
