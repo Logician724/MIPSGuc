662015d321a23e0de1002f596214ace0b628c6e7
Logician724 <king2sairos2013@gmail.com>
2018-04-25 23:00:50 +0200

    feat: Add MEM_RAM control behaviour

--- MEM_RAM.v
+++ MEM_RAM.v
@@ -1,4 +1,4 @@
-module(
+module MEM_RAM(
     read_data,
     mem_write,
     mem_read,
@@ -8,20 +8,45 @@ module(
 
 input mem_read;
 input mem_write;
+input [1:0] load_mode;
 input [31:0] address;
 input [31:0] write_data;
 output reg [31:0] read_data;
 reg [4294967296:0] ram [7:0];
 
 always@(mem_read,mem_write,address,write_data)begin
-    if(mem_read)begin
-        read_data = {ram[address],ram[address+1],ram[address+2],ram[address+3]}
+    if(mem_read && load_mode == 2'b00)begin
+        case(load_mode)
+        2'b00: 
+            read_data = {
+            ram[address],
+            ram[address + 1],
+            ram[address + 2],
+            ram[address + 3]
+            };
+        2'b01:
+            read_data = {
+            {16{ram[address][7]}},
+            ram[address],
+            ram[address + 1]
+            };
+        2'b10:
+        read_data = {
+            16'b0,
+            ram[address],
+            ram[address + 1]
+            };
+        default: $display("Error in MEM_RAM");
+    end
+        
+    if(mem_write)begin
+    {
+        ram[address],
+        ram[address+1],
+        ram[address+2],
+        ram[address+3]} = write_data;
     end
 
-    if(mem_write)begin
-    {ram[address],ram[address+1],ram[address+2],ram[address+3]} = write_data;
-    end
-    
 end
 
 
