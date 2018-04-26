4e6182b87eec0ded375842acb71d18868b097f89
Maqarios <maqarioswagdy@gmail.com>
2018-04-26 16:48:02 +0200

    fix: ins mem is reg & IF compiles

--- IF_Stage.v
+++ IF_Stage.v
@@ -3,18 +3,22 @@ module IF_Stage(
   input [31:0] MUX_OPT_0,               // MUX OPTION 0
   input [31:0] MUX_OPT_1,               // MUX OPTION 1
   input MEM_WRITE,                      // MUX CONTROL SIGNAL
-  input [31:0] INS_MEM [0:16777215],    // INSTRUCTION MEMORY
   output [31:0] NEXT_INS_ADR_OUT,       // NEXT INSTRUCTION ADDRESS OUTPUT
   output [31:0] CUR_INS_OUT             // CURRENT INSTRUCTION OUTPUT
 );
 
 wire [31:0] NEXT_INS_ADR, CUR_INS;
+reg [31:0] MUX_OPT_0_DELAYED, MUX_OPT_1_DELAYED;
+reg MEM_WRITE_DELAYED;
+
+always @(MUX_OPT_0) #5 MUX_OPT_0_DELAYED = MUX_OPT_0;
+always @(MUX_OPT_1) #5 MUX_OPT_1_DELAYED = MUX_OPT_0;
+always @(MEM_WRITE) #5 MEM_WRITE_DELAYED = MUX_OPT_0;
 
 IF_Sel IF_Sel_Module(
   MUX_OPT_0,
   MUX_OPT_1,
   MEM_WRITE,
-  INS_MEM,
   NEXT_INS_ADR,
   CUR_INS
 );
