aef09d9de2284907a806365f4f8db6968982b79a
OmarElkilany <omar.alkilani23@gmail.com>
2018-04-27 20:13:22 +0200

    IF_ID REG REWRITE

--- IF_ID_Reg.v
+++ IF_ID_Reg.v
@@ -1,15 +1,15 @@
 module IF_ID_Reg(
-    input CLK,                          // CLOCK
-    input [31:0] NEXT_INS_ADR_IN,       // NEXT INSTRUCTION ADDRESS INPUT
-    input [31:0] CUR_INS_IN,            // CURRENT INSTRUCTION INPUT
-    output reg [31:0] NEXT_INS_ADR_OUT, // NEXT INSTRUCTION ADDRESS OUTPUT
-    output reg [31:0] CUR_INS_OUT       // CURRENT INSTRUCTION OUTPUT
+	input clk,
+	input [31:0] in_instruction,
+	input [31:0] in_pc_plus_four,
+	output reg [31:0] instruction_out,
+	output reg [31:0] pc_plus_four_out
 );
 
-always @(posedge CLK)
+always @(posedge clk)
 begin
-  NEXT_INS_ADR_OUT <= NEXT_INS_ADR_IN;
-  CUR_INS_OUT <= CUR_INS_IN;
+	instruction_out <= in_instruction;
+	pc_plus_four_out <= in_pc_plus_four;
 end
 
 endmodule
