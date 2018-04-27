58f69044adf513cc9bf13b9b7a681eb4aa48e8d0
OmarElkilany <omar.alkilani23@gmail.com>
2018-04-27 20:08:45 +0200

    IF_StAge rewrite

--- IF_Stage.v
+++ IF_Stage.v
@@ -1,44 +1,29 @@
 module IF_Stage(
-  input CLK,                            // CLOCK
-  input [31:0] MUX_OPT_0,               // MUX OPTION 0
-  input [31:0] MUX_OPT_1,               // MUX OPTION 1
-  input PC_SRC,                      // MUX CONTROL SIGNAL
-  output [31:0] NEXT_INS_ADR_OUT,       // NEXT INSTRUCTION ADDRESS OUTPUT
-  output [31:0] CUR_INS_OUT             // CURRENT INSTRUCTION OUTPUT
+input clk,
+input in_PCSrc,
+input in_branch_address,
+output [31:0] instruction_out,
+output [31:0] pc_plus_four_out
 );
 
-wire [31:0] NEXT_INS_ADR, CUR_INS;
-reg [31:0] MUX_OPT_0_DELAYED, MUX_OPT_1_DELAYED;
-reg PC_SRC_DELAYED;
+reg [31:0] PC = 0;
+reg [31:0] instruction_memory [0:4000]; //adjusted due to constraints by modelsim
 
-// initialization
+integer i = 0;
 initial
 begin
-MUX_OPT_0_DELAYED <= 0;
-MUX_OPT_1_DELAYED <= 0;
-PC_SRC_DELAYED <=0;
+for(i = 0; i < 4000; i = i + 1)
+	instruction_memory[i] = 31'b0;
+	
+instruction_memory[0] = 32'b001000_00000_01001_0000000000000010;
 end
 
-// stage delay
-always @(MUX_OPT_0) #5 MUX_OPT_0_DELAYED = MUX_OPT_0;
-always @(MUX_OPT_1) #5 MUX_OPT_1_DELAYED = MUX_OPT_0;
-always @(PC_SRC) #5 PC_SRC_DELAYED = PC_SRC;
+assign pc_plus_four_out = PC + 4;
+assign instruction_out = instruction_memory[PC];
 
-IF_Sel IF_Sel_Module(
-  CLK,
-  MUX_OPT_0_DELAYED,
-  MUX_OPT_1_DELAYED,
-  PC_SRC_DELAYED,
-  NEXT_INS_ADR,
-  CUR_INS
-);
-
-IF_ID_Reg IF_ID_Reg_Module(
-  CLK,
-  NEXT_INS_ADR,
-  CUR_INS,
-  NEXT_INS_ADR_OUT,
-  CUR_INS_OUT
-);
+always @(posedge clk)
+	begin
+		PC <= (in_PCSrc)? in_branch_address : pc_plus_four_out;
+	end
 
 endmodule
