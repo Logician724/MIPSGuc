b65034806caaa11d5ed0cf061fbd2777167e1d6c
Maqarios <maqarioswagdy@gmail.com>
2018-04-27 14:39:59 +0200

    fix: fixed confusing variable naming

--- IF_Stage.v
+++ IF_Stage.v
@@ -2,23 +2,23 @@ module IF_Stage(
   input CLK,                            // CLOCK
   input [31:0] MUX_OPT_0,               // MUX OPTION 0
   input [31:0] MUX_OPT_1,               // MUX OPTION 1
-  input MEM_WRITE,                      // MUX CONTROL SIGNAL
+  input PC_SRC,                      // MUX CONTROL SIGNAL
   output [31:0] NEXT_INS_ADR_OUT,       // NEXT INSTRUCTION ADDRESS OUTPUT
   output [31:0] CUR_INS_OUT             // CURRENT INSTRUCTION OUTPUT
 );
 
 wire [31:0] NEXT_INS_ADR, CUR_INS;
 reg [31:0] MUX_OPT_0_DELAYED, MUX_OPT_1_DELAYED;
-reg MEM_WRITE_DELAYED;
+reg PC_SRC_DELAYED;
 
 always @(MUX_OPT_0) #5 MUX_OPT_0_DELAYED = MUX_OPT_0;
 always @(MUX_OPT_1) #5 MUX_OPT_1_DELAYED = MUX_OPT_0;
-always @(MEM_WRITE) #5 MEM_WRITE_DELAYED = MUX_OPT_0;
+always @(PC_SRC) #5 PC_SRC_DELAYED = PC_SRC;
 
 IF_Sel IF_Sel_Module(
   MUX_OPT_0,
   MUX_OPT_1,
-  MEM_WRITE,
+  PC_SRC,
   NEXT_INS_ADR,
   CUR_INS
 );
